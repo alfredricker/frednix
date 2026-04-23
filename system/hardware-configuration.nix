9ddd6d0d69e1afa58554a362849068dcc8e0f1cf
alfred ricker <alfred.ricker7@gmail.com>
2026-04-22 22:04:56 -0700

    start audio drivers

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -10,7 +10,7 @@
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" "mt7921e" ];
+  boot.kernelModules = [ "kvm-amd" "mt7921e" "snd_hda_intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
